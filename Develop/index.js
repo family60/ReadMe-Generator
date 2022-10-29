a64f6fa8238189844e4bc64a68465fa3f1134cce
Kush Patel <kushrpatel06@yahoo.com>
2022-10-29 16:50:20 -0400

    Added Questions array for user input Added validation for questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,127 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter project name/title",
+        validate: title => {
+            if (title) {
+                return true;
+            } else {
+                console.log("Please enter a project name/title to contiue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Brief explanation/description of your project",
+        validate: description => {
+            if (description) {
+                return true;
+            } else {
+                console.log("Please enter a description of your project to continue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "installationGuide",
+        message: "Explain how to install this project",
+        validate: installtionGuide => {
+            if (installtionGuide) {
+                return true;
+            } else {
+                console.log("Please enter an explanation to continue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Explain how to use your project",
+        validate: usage => {
+            if (usage) {
+                return true;
+            } else {
+                console.log("You must provide an explanation on how to use your project to continue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: " Enter information on how to contribute to this project",
+        validate: contribute => {
+            if (contribute) {
+                return true;
+            } else {
+                console.log("You must provide an explanation on contribution to continue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How would someone test your project?",
+        validate: test => {
+            if (test) {
+                return true;
+            } else {
+                console.log("Please enter an explanation on how someone would test your project to continue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Choose the license for your project",
+        choices: ["MIT", "GNU-General-Public", "Eclipse-Public-License-2.0", "none"],
+        validate: license => {
+            if (license) {
+                return true;
+            } else {
+                console.log("Please check a box to continue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username",
+        validate: github => {
+            if (github) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username to continue");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email for possible further questioning from other devs",
+        validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log("Please enter your email to finish this process");
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
