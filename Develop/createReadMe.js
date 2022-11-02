52d87516528af7a7bc7ba4b782cc74fb1068893b
Kush Patel <kushrpatel06@yahoo.com>
2022-11-02 11:55:11 -0400

    Edited format to fix potential bug where github doesn't recognize the #'s as titles and subtitles

--- Develop/createReadMe.js
+++ Develop/createReadMe.js
@@ -1,31 +1,31 @@
 function createReadMe(userInput) {
     return `
-    # ${userInput.title}
+# ${userInput.title}
     
-    ## Table of Contents 
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Licensing](#license)
-    - [Contribution](#contribution)
-    - [Testing](#testing)
-    - [Additional Info](#additional-info)
-    ## Description:
-    ${userInput.description}
-    ## Installation:
-    ${userInput.installationGuide}
-    ## Usage:
-    ${userInput.usage}
-    ## License:
-    ${userInput.license}
-    [![license](https://img.shields.io/badge/license-${userInput.license}-blue)](https://shields.io)
-    ## Contribution:
-    ${userInput.contribute}
-    ## Testing:
-    ${userInput.test}
-    ## Additional Info:
-    - Github: [${userInput.github}](https://github.com/${userInput.github})
-    - Email: ${userInput.email} 
-    `;
+## Table of Contents 
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licensing](#license)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Additional Info](#additional-info)
+## Description:
+${userInput.description}
+## Installation:
+${userInput.installationGuide}
+## Usage:
+${userInput.usage}
+## License:
+${userInput.license}
+[![license](https://img.shields.io/badge/license-${userInput.license}-blue)](https://shields.io)
+## Contribution:
+${userInput.contribute}
+## Testing:
+${userInput.test}
+## Additional Info:
+- Github: [${userInput.github}](https://github.com/${userInput.github})
+- Email: ${userInput.email} 
+`;
 }
 module.exports = createReadMe;
